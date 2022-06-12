2ffe95368d5554922e0d4c8b60022f9142ba94aa
dymchenkko <elizavetadymchenko20@gmail.com>
2022-06-12 23:32:08 +0200

    first commit

--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -1,21 +1,32 @@
 #![no_main]
 #![no_std]
 
-use risc0_zkvm_guest::env;
+use risc0_zkvm_guest::{env, sha};
 
 risc0_zkvm_guest::entry!(main);
+use aes_gcm::{Aes256Gcm, Key, Nonce}; // Or `Aes128Gcm`
+use aes_gcm::aead::{Aead, NewAead};
+use core::str::from_utf8;
+use serde::{Deserialize, Serialize};
 
+#[derive(Deserialize, Serialize)]
+pub struct Information<'a> {
+    pub nonce: [u8; 12],
+    pub key: [u8; 32],
+    pub ciphertext: &'a[u8],
+}
 pub fn main() {
-    // Load the first number from the host
-    let a: u64 = env::read();
-    // Load the second number from the host
-    let b: u64 = env::read();
-    // Verify that neither of them are 1 (i.e. nontrivial factors)
-    if a == 1 || b == 1 {
-        panic!("Trivial factors")
-    }
-    // Compute the product
-    let c: u64 = a * b;
-    // Commit it to the public journal
-    env::commit(&c);
+   let info: Information = env::read();
+    let key = Key::from_slice(&info.key);
+    let cipher = Aes256Gcm::new(key);
+    let nonce = Nonce::from_slice(&info.nonce);
+    let ciphertext = cipher.encrypt(nonce, b"plaintext message".as_ref())
+    .expect("encryption failure!");
+    // this ciphertext I need to receive(the same as below), but receive : [116, 0, 0, 0, ...], so below I use just particular array, but I want to take dynamic one from info.ciphertext.
+    let ciphertext = [116, 91, 46, 203, 180, 87, 176, 113, 174, 0, 131, 153, 181, 96, 241, 113, 248, 81, 116, 205, 254, 46, 225, 239, 29, 81, 154, 25, 46, 188, 244, 214, 232];
+    //let plaintext = cipher.decrypt(nonce, info.ciphertext.as_ref()) - this I want to work for me, but info.ciphertext have wrong array
+    let plaintext = cipher.decrypt(nonce, ciphertext.as_ref())
+    .expect("decryption failure!");
+    env::commit(&(&plaintext == b"plaintext message"));
+    
 }
--- starter/src/main.rs
+++ starter/src/main.rs
@@ -4,14 +4,24 @@ use methods::{MULTIPLY_ID, MULTIPLY_PATH};
 use risc0_zkvm_host::Prover;
 use risc0_zkvm_serde::{from_slice, to_vec};
 use tempfile::tempdir;
-
+use aes_gcm::{Aes256Gcm, Key, Nonce}; // Or `Aes128Gcm`
+use aes_gcm::aead::{Aead, NewAead};
+use serde::{Deserialize, Serialize};
+#[derive(Deserialize, Serialize)]
+pub struct Information <'a> {
+    pub nonce: [u8; 12],
+    pub key: [u8; 32],
+    pub ciphertext:&'a [u8],
+}
 fn main() {
-    // Pick two numbers
-    let a: u64 = 17;
-    let b: u64 = 23;
-
-    // Write the ID to a file, this is to work around the fact that the C++
-    // prover API doesn't take IDs as buffers currently.
+    let b = b"unique nonce";
+    let c = b"an example very very secret key.";
+    let key = Key::from_slice(c);
+    let cipher = Aes256Gcm::new(key);
+    let nonce = Nonce::from_slice(b);
+    let ciphertext = cipher.encrypt(nonce, b"plaintext message".as_ref())
+    .expect("encryption failure!");
+    println!("Ciphertext {:?}", &ciphertext);
     let temp_dir = tempdir().unwrap();
     let id_path = temp_dir
         .path()
@@ -20,24 +30,22 @@ fn main() {
         .unwrap()
         .to_string();
     fs::write(&id_path, MULTIPLY_ID).unwrap();
-
-    // Multiply them inside the ZKP
-    // First, we make the prover, loading the 'multiply' method
     let mut prover = Prover::new(&MULTIPLY_PATH, &id_path).unwrap();
-    // Next we send a + b to the guest
-    prover.add_input(to_vec(&a).unwrap().as_slice()).unwrap();
-    prover.add_input(to_vec(&b).unwrap().as_slice()).unwrap();
-    // Run prover + generate receipt
+    let input = Information{
+        nonce: *b,
+        key: *c,
+        ciphertext:&ciphertext,
+    };
+    // I am sure the problem in lifetime (for instance, in key field I tranfer array called 'c' and recieve the same array in the guest environment as in the host,
+    // but when I tried to tranfer array called 'c' in the ciphertext field, I received different array
+    let vec = risc0_zkvm_serde::to_vec(&input).unwrap();
+    prover.add_input(vec.as_slice());
+    println!("{:?}", &input.ciphertext);
+
     let receipt = prover.run().unwrap();
 
-    // Extract journal of receipt (i.e. output c, where c = a * b)
-    let c: u64 = from_slice(&receipt.get_journal_vec().unwrap()).unwrap();
-
-    // Print an assertation
-    println!("I know the factors of {}, and I can prove it!", c);
-
-    // Here is where one would send 'receipt' over the network...
-
-    // Verify receipt, panic if it's wrong
-    receipt.verify(&id_path).unwrap();
+    let c = receipt.get_seal().unwrap();
+    let c: Result<bool, _> = from_slice(c);
+    let c = c.unwrap();
+    println!("{:?}", c);
 }
